e34e7e4d15d58107b8a7b5a22dacdb1a89b99bd7
Joe Baltimore <joebalt@skypeak.com>
2017-02-25 17:46:40 +0000

    Move begin/end output to proper locations.

--- ohlc.py
+++ ohlc.py
@@ -50,7 +50,6 @@ def get_earnings_data(date, symbol="QQQ"):
 
 
 def main(argv):
-    print('Begin run...')
     args = init_argparse()
 
     sleep_times = [0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00]
@@ -71,9 +70,10 @@ def main(argv):
     print('')
     print(el)
 
-    print('End run.')
     return 0
 
 
 if __name__ == "__main__":
+    print('Begin run...')
     sys.exit(main(sys.argv))
+    print('End run.')
